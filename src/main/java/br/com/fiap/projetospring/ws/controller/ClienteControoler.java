2ff6e9d25fd8b712f05ad130241f88e8982065ab
Ygor Gomes Mussa Cury <ygor_gomes@hotmail.com>
2017-10-02 11:50:03 -0300

    servico persistindo no mysql

--- src/main/java/br/com/fiap/projetospring/ws/controller/ClienteControoler.java
+++ src/main/java/br/com/fiap/projetospring/ws/controller/ClienteControoler.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,80 +15,52 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.projetospring.ws.model.Cliente;
+import br.com.fiap.projetospring.ws.service.ClienteService;
 
 @RestController
 public class ClienteControoler {
 	
-	
-	Map<Integer, Cliente> clientes = new HashMap<>();
-	Integer proximoId = 0;
-	
-	//Negocios
-	private Cliente cadastrar(Cliente cliente){
-		
-		
-		cliente.setId(proximoId);
-		//criar id
-		proximoId++;
-		clientes.put(cliente.getId(), cliente);
-		
-		return cliente;
-	}
-	
-	private Collection<Cliente> buscarTodos(){
-		return  clientes.values();
-	}
-	
-	public void excluir(Cliente cliente){
-		clientes.remove(cliente.getId());
-	}
-	
-	private Cliente buscarPorId(Integer id){
-		return clientes.get(id);
-	}
-	
-	private Cliente alterar(Cliente cliente){
-		clientes.put(cliente.getId(), cliente);
-		return cliente;
-	}
+	@Autowired
+	ClienteService clienteService;
 	
 	
 	//End points
+	//cadastrar
 	@RequestMapping(method=RequestMethod.POST, value="/clientes" , consumes=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente){
 		
-		Cliente clienteCadastrado = cadastrar(cliente);
+		Cliente clienteCadastrado = clienteService.cadastrar(cliente);
 		System.out.println("Cliente "+cliente.getNome()+" cadastrada com sucesso!");
 		return new ResponseEntity<Cliente>(clienteCadastrado, HttpStatus.CREATED);
 		
 		
 	}
-	
+	//buscar
 	@RequestMapping(method=RequestMethod.GET, value="/clientes", produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Collection<Cliente>> buscarTodosClientes(){
 		
-		Collection<Cliente> clientesBuscados = buscarTodos();
+		Collection<Cliente> clientesBuscados = clienteService.buscarTodos();
 		return new ResponseEntity<>(clientesBuscados, HttpStatus.OK);
 		
 	}
-	
+	//excluir
 	@RequestMapping(method=RequestMethod.DELETE, value="/clientes/{id}")
 	public ResponseEntity<Cliente> excluirCliente(@PathVariable Integer id){
 		
-		Cliente clienteEncontrado = buscarPorId(id);
+		Cliente clienteEncontrado = clienteService.buscarPorId(id);
 		
 		if(clienteEncontrado == null){
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
-		excluir(clienteEncontrado);
+		clienteService.excluir(clienteEncontrado);
 		System.out.println("Cliente "+clienteEncontrado.getNome()+" removida com sucesso!");
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
-	
+	//alterar
 	@RequestMapping(method=RequestMethod.PUT, value="/clientes" , consumes=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Cliente> alterarCliente(@RequestBody Cliente cliente){
 		
-		Cliente clienteAlterado = alterar(cliente);
+		Cliente clienteAlterado = clienteService.alterar(cliente);
 		System.out.println("Cliente "+cliente.getNome()+" alterado com sucesso!");
 		return new ResponseEntity<>(clienteAlterado, HttpStatus.OK);
 		
--- src/main/java/br/com/fiap/projetospring/ws/service/ClienteService.java
+++ src/main/java/br/com/fiap/projetospring/ws/service/ClienteService.java
@@ -1,5 +1,49 @@
 package br.com.fiap.projetospring.ws.service;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.fiap.projetospring.ws.model.Cliente;
+import br.com.fiap.projetospring.ws.repository.ClienteRepository;
+
+@Service
 public class ClienteService {
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	private Map<Integer, Cliente> clientes = new HashMap<>();
+	private Integer proximoId = 0;
+	
+	//Negocios
+	
+	public Cliente cadastrar(Cliente cliente){
+		/**cliente.setId(proximoId);
+		//criar id
+		proximoId++;
+		clientes.put(cliente.getId(), cliente); **/
+		
+		return clienteRepository.save(cliente);
+	}
+	
+	public Collection<Cliente> buscarTodos(){
+		return  clienteRepository.findAll();
+	}
+	
+	public void excluir(Cliente cliente){
+		clienteRepository.delete(cliente);
+	}
+	
+	public Cliente buscarPorId(Integer id){
+		return clienteRepository.findOne(id);
+	}
+	
+	public Cliente alterar(Cliente cliente){
+		return clienteRepository.save(cliente);
+	}
 
 }
